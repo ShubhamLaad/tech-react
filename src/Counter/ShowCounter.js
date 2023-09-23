62a9bd3cc866385b2d6b8c8ef2afa0528bc22c87
Shubham Laad <shubhamlaad666@gmail.com>
2023-09-23 12:16:53 +0530

    form input

--- src/Counter/ShowCounter.js
+++ src/Counter/ShowCounter.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 export function ShowCounter({ counter, handleClick }) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div>
       {' '}
+      <input placeholder="Enter no" value={value} onChange={handleChange} />
       <h2>{counter}</h2>
       <button onClick={() => handleClick('+')}>+</button>
       <button onClick={() => handleClick('-')}>-</button>
