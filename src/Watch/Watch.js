e14adea337c0e77a14b4a7a39f8d0dbee22efc81
Shubham Laad <shubhamlaad666@gmail.com>
2023-10-07 13:38:40 +0530

    counter chnages

--- src/Watch/Watch.js
+++ src/Watch/Watch.js
@@ -4,14 +4,14 @@ export function Watch() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    const timoutOut = setTimeout(() => {
+    const timoutOut = setInterval(() => {
       setTime(time + 1);
     }, 1000);
 
     return () => {
       clearTimeout(timoutOut);
     };
-  }, [time]);
+  }, []);
 
   return (
     <div>
